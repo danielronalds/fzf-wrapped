fe915402df361d8caea607f4d0c98fb3e64f1406
Daniel R <nzdanron@gmail.com>
2023-10-31 20:25:28 +1300

    Added prompt field

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-pub struct Fzf {}
+pub struct Fzf {
+    prompt: String,
+}
 
 impl Fzf {
     pub fn builder() -> FzfBuilder {
@@ -6,15 +8,29 @@ impl Fzf {
     }
 }
 
-pub struct FzfBuilder {}
+pub struct FzfBuilder {
+    prompt: String,
+}
 
 impl Default for FzfBuilder {
     fn default() -> Self {
-        Self {}
+        Self { prompt: "> ".to_string() }
     }
 }
 
-impl FzfBuilder {}
+impl FzfBuilder {
+    /// The prompt in the search bar
+    pub fn prompt<T: Into<String>>(&mut self, prompt: T) -> &mut Self {
+        self.prompt = prompt.into();
+        self
+    }
+
+    pub fn build(self) -> Fzf {
+        Fzf {
+            prompt: self.prompt,
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {}
